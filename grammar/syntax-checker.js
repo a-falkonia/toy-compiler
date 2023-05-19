2732fa875437a54fe06da86ea0d2982b625c5356
Anna <a.sokolova@stream-telecom.ru>
2023-05-19 17:36:35 +0300

    chore(project): remove ohm boilerplates

--- grammar/syntax-checker.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const ohm = require('ohm-js');
-
-const grammar = ohm.grammar(fs.readFileSync('grammar/astro.ohm'));
-
-module.exports = text => grammar.match(text).succeeded();
